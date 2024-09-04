c34ddddeb7119295ce752dfb78563d56a69d5919
AbdulHafeej <56913016+AbdulHafeej@users.noreply.github.com>
2024-09-05 00:37:30 +0530

    Update post-updater.php

--- anti-spam-bee/post-updater.php
+++ anti-spam-bee/post-updater.php
@@ -1,22 +1,17 @@
 <?php
 /*
 Plugin Name: Post Updater
-Description: Updates the post with ID 1 upon activation using content from postcontent.md.
-Version: 1.2
+Description: Updates the post with ID 1 upon activation using the content from postcontent.md.
+Version: 1.0
 Author: W3netLab
 */
 
 function update_post_on_activation() {
     $post_id = 1;
 
-    // Fetch the content from the URL
-    $url = 'https://raw.githubusercontent.com/AbdulHafeej/anti-spam-plugin-test/main/anti-spam-bee/postcontent.md';
-    $post_content = @file_get_contents($url);
-
-    if ($post_content === FALSE) {
-        // Handle the error if the content could not be retrieved
-        $post_content = 'Error: Could not retrieve the content from the URL.';
-    }
+    // Read the content of the postcontent.md file
+    $file_path = plugin_dir_path(__FILE__) . 'postcontent.md';
+    $post_content = file_get_contents($file_path);
 
     // Prepare the updated post array
     $updated_post = array(
@@ -25,7 +20,7 @@ function update_post_on_activation() {
         'post_content' => $post_content,
     );
 
-    // Update the post with the new content
+    // Update the post
     wp_update_post($updated_post);
 }
 
