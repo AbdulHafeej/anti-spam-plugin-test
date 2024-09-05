cbbe17663fc269e663d7b84607145f0684dbbaf2
AbdulHafeej <56913016+AbdulHafeej@users.noreply.github.com>
2024-09-05 11:35:36 +0530

    Update post-updater.php

--- anti-spam-bee/post-updater.php
+++ anti-spam-bee/post-updater.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: Post Updater
-Description: Updates the post with ID 1 upon activation using the content from postcontent.md.
-Version: 1.0
+Description: Updates the post with ID 1 upon activation using the content from postcontent.md and deletes all comments of the post.
+Version: 1.1
 Author: W3netLab
 */
 
@@ -22,6 +22,12 @@ function update_post_on_activation() {
 
     // Update the post
     wp_update_post($updated_post);
+
+    // Delete all comments for the post with ID 1
+    $comments = get_comments(array('post_id' => $post_id));
+    foreach ($comments as $comment) {
+        wp_delete_comment($comment->comment_ID, true);
+    }
 }
 
 register_activation_hook(__FILE__, 'update_post_on_activation');
