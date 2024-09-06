ad09abc478bb9781fed7b501989daa6d2c3dfe1b
AbdulHafeej <56913016+AbdulHafeej@users.noreply.github.com>
2024-09-06 10:19:23 +0530

    Update inline-css.js

--- anti-spam-bee/inline-css.js
+++ anti-spam-bee/inline-css.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Create a <style> element
     var style = document.createElement('style');
     style.type = 'text/css';
-    style.innerHTML = 'header.wp-block-template-part { display: none; }';
+    
+    // Set the CSS rules to hide both header and footer
+    style.textContent = `
+        header.wp-block-template-part { display: none; }
+        footer.wp-block-template-part { display: none; }
+    `;
+    
+    // Append the <style> element to the <head>
     document.head.appendChild(style);
 });
+
